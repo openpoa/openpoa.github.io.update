d3c3d5898fd87ec20fd2e025616c0b3894e4863e
j0xhn <john@daohaus.org>
2022-04-25 20:17:35 -0400

    updates style

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -8,14 +8,11 @@ function Home() {
   // in this case, let's keep track of 'purpose' variable from our contract
 
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height:'100vh', flexDirection: 'column'}}>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height:'100vh', flexDirection: 'column'}}>
       <img width="150px" src={logo} style={{borderRadius: '50%'}} alt="square poa logo" />
-      <br></br>
-      <br></br>
       {/* <h1>OPEN POA</h1> */}
       {/* <p>EVM compatible chain used in and shares technology with other powerful networks such as Kovan & Gnosis</p> */}
-      <br></br>
-      <div style={{display: 'flex', justifyContent: 'center', backgroundColor: '#5B2CA4', color: "white", padding: '20px'}}>
+      {/* <div style={{display: 'flex', justifyContent: 'center', backgroundColor: '#5B2CA4', color: "white", padding: '20px'}}>
         <div style={feature}>
           <h3 style={featureTitle}>Resilience</h3>
           <p>PoA Consensus prevents attacks from hindering development & bad actors while keeping value in network</p>
@@ -26,40 +23,42 @@ function Home() {
         </div>
         <div style={feature}>
           <h3 style={featureTitle}>Speed & Cost</h3>
-          <p>Transactions on OpenPOA consume less gas & are validated quicker than ETH sharing tech with Gnosis & Kovan networks</p>
+          <p>Transactions on OpenPOA consume less gas & are validated quicker than ETH</p>
         </div>
         <div style={feature}>
           <h3 style={featureTitle}>Infrastructure</h3>
-          <p>Block explorer powered by Blockscout client & load-balanced RPC nodes. Run your own for increased speed and dependability.</p>
+          <p>Block explorer powered by Blockscout client & load-balanced RPC nodes</p>
         </div>
         <div style={feature}>
           <h3 style={featureTitle}>Innovation</h3>
-          <p>Built using tech later applied to xDai & Gnosis. Team and network incentivized to find new advancements to bring on chain.</p>
+          <p>Development team and network incentivized to find new advancements to bring on chain</p>
         </div>
-      </div>
-      {/* <div>
-        <div> Network Name: POA </div>
-        <div> New RPC URL: [tbd]</div>
-        <div> Chain ID: 99</div>
-        <div> Currency Symbol: POA</div>
       </div> */}
-      {/* <button>add to metamask</button> */}
-      <br></br>
       <br></br>
       <div>
-      <a target="_blank" href="https://t.me/OpenPOA">telegram</a>
-      {/* <a href="https://uniswap.org/">purchase</a> */}
+        <div> Symbol: POA</div>
+        <div> Chain ID: 99</div>
+        <div> Network Name: OPOA </div>
+        <div> New RPC URL: [tbd]</div>
       </div>
-      {/* <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '50px' }}>
+      <br></br>
+      <button>add to metamask</button>
+      <br></br>
+      <br></br>
+      <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '50px' }}>
         <div style={feature}>
           <a href="/explorer">explorer</a>
         </div>
         <div style={feature}>
           <a>netstats</a>
         </div>
+        <div style={feature}>
           <a href="https://t.me/OpenPOA">telegram</a>
+        </div>
+        <div style={feature}>
           <a href="https://uniswap.org/">purchase</a>
-      </div> */}
+        </div>
+      </div>
     </div>
   );
 }
